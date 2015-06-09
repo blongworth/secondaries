b4ae3d54bf2bcd2c931a410714278b33c3b5a819
Brett Longworth <blongworth@gmail.com>
2015-06-09 16:48:36 -0400

    Add data for counts

--- CalculateSecondaries.R
+++ CalculateSecondaries.R
@@ -72,6 +72,7 @@ getSecondary <- function (rec, from, to, sys, db) {
         SELECT snics_raw.tp_num, AVG(le12c) AS le12c
         FROM snics_raw, target
         WHERE target.tp_num = snics_raw.tp_num
+          AND ok_calc = 1
           AND target.rec_num =", rec," 
           ",whid, "
   	      AND target.tp_date_pressed > '",from,"'
@@ -79,7 +80,61 @@ getSecondary <- function (rec, from, to, sys, db) {
           GROUP BY snics_raw.tp_num
         "))
   
-  left_join(f, cur, by = "tp_num")
+count <- sqlQuery(db, paste("
+        SELECT snics_raw.tp_num, SUM(cnt_14c) AS counts
+        FROM snics_raw, target
+        WHERE target.tp_num = snics_raw.tp_num
+          AND ok_calc = 1
+          AND target.rec_num =", rec," 
+          ",whid, "
+          AND target.tp_date_pressed > '",from,"'
+          ", ts, "
+          GROUP BY snics_raw.tp_num
+        "))
+
+  f <- left_join(f, cur, by = "tp_num")
+  left_join(f, count, by = "tp_num")
+}
+
+getOldSecondary <- function (rec, from, to, sys, db) { 
+  #Get data for a secondary from database
+  #Return query result as data table
+  
+#   #What system do we want data for?
+#   if (sys == "cfams") {
+#     whid <- "AND wheel_id LIKE 'C%'"
+#   } else if (sys =="usams") {
+#     whid <- "AND wheel_id LIKE 'U%'"
+#   } else if (sys =="both") {
+#     whid <- "" 
+#   } else {
+#     whid <- "AND wheel_id NOT LIKE 'C%'"
+#   }
+  
+  #Data to present or provided end date
+  if (to != "present") {
+    ts <- paste("AND target.tp_date_pressed < '", to,"' ")
+  } else {
+    ts <- ""
+  }
+  
+  #Do the query
+  sqlQuery(db, paste("
+                          SELECT target.rec_num, target.tp_num, target.osg_num, target.target_name, 
+                          wheel_pos.wheel_id, target.tp_date_pressed, graphite_lab.lab_name, 
+                          no_os.f_modern, no_os.f_int_error, no_os.f_ext_error, no_os.dc13,
+                          graphite.gf_co2_qty, no_os.q_flag
+                          FROM no_os, target, wheel_pos, graphite, graphite_lab
+                          WHERE target.tp_num = no_os.tp_num 
+                          AND target.tp_num = wheel_pos.tp_num 
+                          AND target.tp_num = snics_results.tp_num
+                          AND target.osg_num = graphite.osg_num 
+                          AND target.graphite_lab = graphite_lab.lab_id
+                          AND target.rec_num =", rec," 
+                          AND target.tp_date_pressed > '",from,"'
+                          ", ts, "
+                          "))
+  
   
 }
 
